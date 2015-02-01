75c2002507741bf984be47a25e0ad702b30042fd
Charles Griffin <cgriffin@indatus.com>
2015-02-01 17:48:56 -0500

    memory allocation example

--- src/test.c
+++ src/test.c
@@ -14,13 +14,20 @@
 int main(void)
 {
 
-	int sizes[] = {15, 20, 30};
+	int *dynamicArray = malloc( 20 * sizeof(int) );
 
-	int *ptr = sizes;
+	*dynamicArray = 20;
 
-	ptr++;
+	printf("Address is %x and value is %d\n", dynamicArray, *dynamicArray);
 
+	dynamicArray[1] = 30;
+	printf("dynamicArray[1] is %d\n", dynamicArray[0]);
+	printf("Size of dynamicArray is %d\n", sizeof(dynamicArray));
 
-	printf("%d", *ptr);
+	printf("ads %d", dynamicArray[0]);
+	free(dynamicArray);
+	printf("adsf %s", dynamicArray[0]);
+
+	return 0;
 
 }
